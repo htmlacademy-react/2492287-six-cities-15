d19785b23ef9e22dddee24376d9e509fbc147b24
boris <karapuz101@mail.ru>
2024-02-18 23:16:18 +0600

    чуть упростил

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,23 +1,19 @@
 import {Navigate} from 'react-router-dom';
 import AuthorizationStatus from '../../shared/AuthorizationStatus';
 import AppRoute from '../../shared/AppRoute';
-import {PropsWithChildren, ReactElement } from 'react';
+import {PropsWithChildren } from 'react';
 
 type PrivateRouteProps = {
   authorizationStatus: AuthorizationStatus;
 }
 
-function PrivateRoute(props: PropsWithChildren<PrivateRouteProps>): ReactElement {
+function PrivateRoute(props: PropsWithChildren<PrivateRouteProps>): JSX.Element {
   const {authorizationStatus, children} = props;
 
   return (
-    <div>
-      {
-        authorizationStatus === AuthorizationStatus.Auth
-          ? children
-          : <Navigate to={`/${AppRoute.Login}`} />
-      }
-    </div>
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children as JSX.Element
+      : <Navigate to={`/${AppRoute.Login}`} />
   );
 }
 
