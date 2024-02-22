0efbf5152574b4de94dcbad7bcbdfe299d83cb3f
boris <karapuz101@mail.ru>
2024-02-22 23:16:32 +0600

    logo + consts

--- src/const.ts
+++ src/const.ts
@@ -9,13 +9,16 @@ export enum OfferTypes {
   Apartment = 'Apartment'
 }
 
-export enum City {
-  Amsterdam = 'Amsterdam',
-  Cologne = 'Cologne',
-  Paris = 'Paris',
-  Brussels = 'Brussels',
-  Hamburg = 'Hamburg',
-  Dusseldorf = 'Dusseldorf'
+export type TCity = {
+  id: number;
+  name: string;
+  lat: number;
+  lng: number;
+};
+
+export enum LogoLocation {
+  Header = 'Header',
+  Footer = 'Footer'
 }
 
 export type TOffer = {
@@ -23,11 +26,13 @@ export type TOffer = {
   price: number;
   title: string;
   offerType: OfferTypes;
-  ratingPercent: number;
+  rating: number;
   imageName: string;
-  city: City;
+  cityId: number;
   isBookmark?: boolean;
   isPremium?: boolean;
+  lat: number;
+  lng: number;
 }
 
 export type TReview = {
@@ -40,3 +45,26 @@ export type TReview = {
   offerId: number;
 }
 
+export const URL_MARKER_DEFAULT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/pin.svg';
+
+export const URL_MARKER_CURRENT =
+  'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+
+export type TPoint = Pick<TOffer, 'title' | 'lat' | 'lng'>;
+
+export const cities: TCity[] = [
+  {id: 1, name: 'Amsterdam', lat: 52.3909553943508, lng: 4.85309666406198},
+  {id: 2, name: 'Cologne', lat: 52.3909553943508, lng: 4.85309666406198},
+  {id: 3, name: 'Paris', lat: 52.3909553943508, lng: 4.85309666406198},
+  {id: 4, name: 'Brussels', lat: 52.3909553943508, lng: 4.85309666406198},
+  {id: 5, name: 'Hamburg', lat: 52.3909553943508, lng: 4.85309666406198},
+  {id: 6, name: 'Dusseldorf', lat: 52.3909553943508, lng: 4.85309666406198}
+];
+
+export enum OfferSortType{
+  Popular = 'Popular',
+  PriceLowHigh = 'Price: low to high',
+  PriceHighLow = 'Price: high to low',
+  TopRated = 'Top rated first'
+}
