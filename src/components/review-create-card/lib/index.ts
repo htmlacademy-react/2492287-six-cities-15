583b6babf95e59c9d6cbb56a24d7528ee496197c
boris <karapuz101@mail.ru>
2024-03-06 00:34:58 +0600

    fix lint

--- src/components/review-create-card/lib/index.ts
+++ src/components/review-create-card/lib/index.ts
@@ -1,1 +1,2 @@
-export const validateSubmit = (value: string, rating: number) => value.length >= 50 && value.length <= 300 && rating >=1 && rating <=5;
+export const validateSubmit = (value: string, rating: number) =>
+  value.length >= 50 && value.length <= 300 && rating >= 1 && rating <= 5;
--- src/store/api-action.ts
+++ src/store/api-action.ts
@@ -114,7 +114,6 @@ export const fetchReviewsAction = createAsyncThunk<void, string, TActionUtils>(
 export const addReviewAction = createAsyncThunk<void, TReview, TActionUtils>(
   'data/addReview',
   async (review, {dispatch, extra: api}) => {
-    console.log(`${ApiRoute.Comments}/${review.offerId}/${review.rating}`)
     const {data} = await api.post<TReviewFull>(`${ApiRoute.Comments}/${review.offerId}`,
       {comment: review.comment, rating: review.rating});
     dispatch(addReview(data));
